6baa38080273523cfab3945146e920a202b1b650
Chidi Nkwocha <chidi.nkwocha@54gene.com>
2020-11-22 10:48:27 +0100

    Add HDWallet provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,43 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import HAG from "./contracts/HAG.json";
+import HAGTokenSale from "./contracts/HAGTokenSale.json";
+import KycContract from "./contracts/KycContract.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { loaded: false, kycAddress: null };
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      this.web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      this.accounts = await this.web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+      this.networkId = await this.web3.eth.net.getId();
+    
+      this.HAGInstance = new this.web3.eth.Contract(
+        HAG.abi,
+        HAG.networks[this.networkId] && HAG.networks[this.networkId].address,
+      );
+
+      this.HAGTokenSaleInstance = new this.web3.eth.Contract(
+        HAGTokenSale.abi,
+        HAGTokenSale.networks[this.networkId] && HAGTokenSale.networks[this.networkId].address,
+      );
+
+      this.KycContractInstance = new this.web3.eth.Contract(
+        KycContract.abi,
+        KycContract.networks[this.networkId] && KycContract.networks[this.networkId].address,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ loaded: true });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,36 +47,33 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  handleInput = (event) => {
+    const { value } = event.target;
+    this.setState({ kycAddress: value });
+  }
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+  handleKycWhitelisting = async () => {
+    const { kycAddress } = await this.state;
+    await this.KycContractInstance.methods.setKycCompleted(kycAddress).send({ from: this.accounts[0] });
+    alert(`${kycAddress} has been successfully whitelisted.`)
+  }
 
   render() {
-    if (!this.state.web3) {
+    const { loaded, kycAddress } = this.state;
+    if (!loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>HaggleX Token Sale</h1>
+        <p>KYC Whitelisting.</p>
+        <div class="form-group">
+          <label for="">Enter your ethereum address</label>
+          <input type="text" class="form-control" name="kycAddress" id="kycAdrress" value={kycAddress} onChange={this.handleInput}/>
+        </div>
+        <button class="btn" onClick={this.handleKycWhitelisting}>
+          Submit    
+        </button>
       </div>
     );
   }
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+require("dotenv").config({path: "./.env"});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Mnemonics = process.env.MNEMONICS;
+const AccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +13,12 @@ module.exports = {
       port: 7545,
       host: "127.0.0.1",
       network_id: 5777
+    },
+    ganache_local: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider(Mnemonics, "http://127.0.0.1:7545", AccountIndex),
+        network_id: 5777,
     }
   },
   compilers: {
