0ed06c089eee49a204afa8f12c5eea36b7a9c7a9
Chidi Nkwocha <chidi.nkwocha@54gene.com>
2020-11-23 10:25:28 +0100

    Deploy contract to rinkeby testnet

--- contracts/HAG.sol
+++ contracts/HAG.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract HAG is ERC20, ERC20Detailed {
-    constructor(uint256 initialSupply) ERC20Detailed("HaggTest Token", "HAGXTS", 0) public {
+    constructor(uint256 initialSupply) ERC20Detailed("LightBlocks Token", "LTBS", 0) public {
         _mint(msg.sender, initialSupply);
     }
 }
